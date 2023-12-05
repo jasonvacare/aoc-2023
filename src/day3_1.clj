cbd9c36dc8dca425f63c4e4105041c22b6efee66
Jason Vacare <jason.vacare@gmail.com>
2023-12-05 10:47:30 -0500

    day 3 progress

--- src/day3_1.clj
+++ src/day3_1.clj
@@ -1,3 +1,19 @@
+(ns day3_1
+  (:require [clojure.string :as str]))
+
+(defn re-seq-pos [pattern string]
+  (let [match (re-matcher pattern string)]
+    ((fn step []
+       (when (. match find)
+         (cons {:start (. match start) :end (. match end) :group (. match group)}
+           (lazy-seq (step))))))))
+
+(def input
+  (str/split-lines (slurp "resources/day3.txt")))
+
+(def possible-parts (map #(re-seq-pos #"(\d+)" %) input))
+
+(def symbols (map #(re-seq-pos #"[^\.0-9]" %) input))
 
 
-  
+
