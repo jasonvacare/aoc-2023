3d12ed21fded4714d74ab252f6c3ddab346347fb
Jason Vacare <jason.vacare@gmail.com>
2023-12-06 17:15:38 -0500

    day 3 progress

--- src/day3_1.clj
+++ src/day3_1.clj
@@ -1,19 +1,30 @@
 (ns day3_1
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :refer [union]]))
 
-(defn re-seq-pos [pattern string]
+(defn re-seq-pos [pattern string index]
   (let [match (re-matcher pattern string)]
     ((fn step []
        (when (. match find)
-         (cons {:start (. match start) :end (. match end) :group (. match group)}
+         (cons {:start (. match start) :end (. match end) :group (. match group) :index index}
            (lazy-seq (step))))))))
 
 (def input
   (str/split-lines (slurp "resources/day3.txt")))
 
-(def possible-parts (map #(re-seq-pos #"(\d+)" %) input))
+(map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)
+(first (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))
+(second (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))
+(into #{} (first (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)))
+(into #{} (second (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)))
+(union 
+ (into #{} (first (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)))
+ (into #{} (second (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))))
 
-(def symbols (map #(re-seq-pos #"[^\.0-9]" %) input))
+
+
+; (def possible-parts (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))
+; (def symbols (map-indexed (fn [index value] (re-seq-pos #"[^\.0-9]" value index)) input))
 
 
 
