db55eb60bb84cba2454ce43307aab124b700325b
Jason Vacare <jason.vacare@gmail.com>
2023-12-09 00:49:21 -0500

    day 3 progress

--- src/day3_1.clj
+++ src/day3_1.clj
@@ -1,30 +1,33 @@
 (ns day3_1
-  (:require [clojure.string :as str]
-            [clojure.set :refer [union]]))
-
-(defn re-seq-pos [pattern string index]
-  (let [match (re-matcher pattern string)]
-    ((fn step []
-       (when (. match find)
-         (cons {:start (. match start) :end (. match end) :group (. match group) :index index}
-           (lazy-seq (step))))))))
+  (:require [clojure.string :as str]))
 
 (def input
   (str/split-lines (slurp "resources/day3.txt")))
 
-(map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)
-(first (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))
-(second (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))
-(into #{} (first (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)))
-(into #{} (second (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)))
-(union 
- (into #{} (first (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input)))
- (into #{} (second (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))))
+(defn re-seq-part [string line-number]
+  (let [match (re-matcher #"(\d+)" string)]
+    ((fn step []
+       (when (. match find)
+         (cons {:line-number line-number :index (. match start) :length (- (. match end) (. match start)) :end (. match end) :part-number (parse-long (. match group))}
+               (lazy-seq (step))))))))
 
+(defn re-seq-symbol [string line-number]
+  (let [match (re-matcher #"[^\.0-9]" string)]
+    ((fn step []
+       (when (. match find)
+         (cons {:line-number line-number :index (. match start)}
+               (lazy-seq (step))))))))
 
+; parse out a list of potential parts and symbols
+(def possible-parts (reduce into #{} (map-indexed (fn [index value] (re-seq-part value index)) input)))
+(def symbols (reduce into #{} (map-indexed (fn [index value] (re-seq-symbol value index)) input)))
 
-; (def possible-parts (map-indexed (fn [index value] (re-seq-pos #"(\d+)" value index)) input))
-; (def symbols (map-indexed (fn [index value] (re-seq-pos #"[^\.0-9]" value index)) input))
+; index-1, start through end-1
+; index, start-1, end
+; index+1, start through end-1
+
+(def test-part {:line-number 35 :index 103 :length 3 :end 106 :part-number 357})
+
 
 
 
